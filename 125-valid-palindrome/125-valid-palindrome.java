589cb387ec472040e17e7ac1fbc4686c44c8d856
Yeoh Shun Bin <yshunbin@gmail.com>
2022-05-15 16:59:44 +0800

    Time: 3 ms (94.66%), Space: 43.4 MB (68.99%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
         for (int i=0, j=s.length()-1; i<j; i++, j--) {
-            while (i<j && !Character.isLetterOrDigit(s.charAt(i))) {
+            while (i<j && !Character.isLetterOrDigit(s.charAt(i))) { //if it's space
                 i++;
             }
-            while (i<j && !Character.isLetterOrDigit(s.charAt(j))) {
+            while (i<j && !Character.isLetterOrDigit(s.charAt(j))) { //if it's space
                 j--;
             }
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
