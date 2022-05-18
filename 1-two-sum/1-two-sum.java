3672362c90b30f1f060124a2aa78e894b8627402
Yeoh Shun Bin <yshunbin@gmail.com>
2022-05-18 14:09:16 +0800

    Update 1-two-sum.java

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) { //nums = [3,2,4], target = 6
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
@@ -7,7 +7,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[] { i, map.get(complement) };
+                return new int[] { i, map.get(complement) }; //[1,2]
             }
         }
         return null;
