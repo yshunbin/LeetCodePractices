98b64d1a1c16500bd7c3ab3131beea9834639078
Yeoh Shun Bin <yshunbin@gmail.com>
2022-05-24 10:47:52 +0800

    Time: 1 ms (65.74%), Space: 51 MB (54.45%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -2,7 +2,7 @@ class Solution { //[1, 3, 4] 4*5 / 2 = 10
     public int missingNumber(int[] nums) { //[0,1,3] = expectedSum 6
         int n = nums.length;
         int expectedSum = n*(n+1)/2; //if starts with 0
-      //int expectedSum = [(nums.length+1)*(nums.length+2)]/2; if starts with 1
+      //int expectedSum = [(n+1)*(n+2)]/2; if starts with 1
         int actualSum = 0;
         for (int num : nums) 
             actualSum += num;
