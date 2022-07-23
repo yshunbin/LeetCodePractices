a6f5c35d90c53aa8bab44ef376dcb8db73eb8587
Yeoh Shun Bin <yshunbin@gmail.com>
2022-07-23 22:53:41 +0800

    Time: 1 ms (25.79%), Space: 43.5 MB (26.66%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    } else {
+      int left_height = maxDepth(root.left);
+      int right_height = maxDepth(root.right);
+      return Math.max(left_height, right_height) + 1;
+    }
+  }
+}
+//recursion DFS: time - O(n), space - O(log(n))
