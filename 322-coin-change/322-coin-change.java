4ec66273ccbe4da593046ecfe013f97149e98552
Yeoh Shun Bin <yshunbin@gmail.com>
2022-09-05 15:13:42 +0800

    Time: 18 ms (90.54%), Space: 41.6 MB (99.38%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int max = amount+1;
+        int[] dp = new int[max];
+        Arrays.fill(dp,max);
+        dp[0] = 0;
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i-coins[j]] + 1, dp[i]);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
+//let S be amount 
+//dp - btm up: time - O(S*n), space - O(S)
