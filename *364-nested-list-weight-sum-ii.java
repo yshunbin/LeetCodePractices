cc01fe30029bc212b794e7ea567eefb57a2b682c
Shun Bin Yeoh <yshunbin@gmail.com>
2022-09-11 15:05:10 +0800

    Create *364-nested-list-weight-sum-ii.java

---
+++ *364-nested-list-weight-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int depthSumInverse(List<NestedInteger> nestedList) {
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+        
+        int d=1, maxDepth=0, sumOfElements=0, sumOfProducts=0;
+        
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            maxDepth = Math.max(maxDepth, d);
+            for(int i=0; i<size; i++){
+                NestedInteger nested = queue.poll();
+                if(nested.isInteger()){
+                    sumOfElements += nested.getInteger();
+                    sumOfProducts += nested.getInteger() * d;
+                } else {
+                    queue.addAll(nested.getList());
+                }
+            }
+            d++;
+        }
+        return (maxDepth+1) * sumOfElements - sumOfProducts;
+    }
+}
+//single pass bfs: time - O(n), space - O(n)
