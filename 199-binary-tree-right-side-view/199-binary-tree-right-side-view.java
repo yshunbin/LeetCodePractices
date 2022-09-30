7ca7f65b15b3fd3002766613bcd37dddc8091bea
Shun Bin Yeoh <yshunbin@gmail.com>
2022-09-30 13:13:24 +0800

    Time: 0 ms (100.00%), Space: 41.1 MB (87.90%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -2,22 +2,20 @@ class Solution {
     List<Integer> rightSide = new ArrayList<>();
     
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) return rightSide;
+        if(root == null) return rightSide;
         
         helper(root, 0);
         return rightSide;
     }
     
     private void helper(TreeNode node, int level){
-        if(level == rightSide.size()) {
+        if(level == rightSide.size()) 
             rightSide.add(node.val);
-        }
-        if(node.right != null) {
+        if(node.right != null) 
             helper(node.right, level+1);
-        }
-        if(node.left != null) {
+        if(node.left != null)
             helper(node.left, level+1);
-        }
     }
+
 }
 //recursive DFS: time - O(n), space - O(h)
